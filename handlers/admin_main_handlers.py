8d4999b0c2c87719ec894c559d24bfd50e854ad3
antONget <a.l.ponomare@mail.ru>
2024-02-19 10:37:02 +0300

    git init

--- handlers/admin_main_handlers.py
+++ handlers/admin_main_handlers.py
@@ -39,7 +39,7 @@ async def process_start_command_superadmin(message: Message, bot: Bot) -> None:
     create_table_users()
     add_super_admin(id_admin=message.chat.id, user_name=message.from_user.username)
     # запускаем процесс обновления таблицы
-    scheduler.add_job(update_operator, 'cron', hour=8, minute=0, second=0)
+    scheduler.add_job(update_operator, 'cron', hour=10, minute=45, second=0)
     # scheduler.add_job(update_operator, 'interval', seconds=60*5)
     scheduler.add_job(sendler_question, 'interval', seconds=60*30, args=(bot,))
     scheduler.start()
